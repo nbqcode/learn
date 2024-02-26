08530b71e7ce2a857a8e6f735c9d45f908192ce7
John Doe <309865580@qq.com>
2024-02-27 04:07:39 +0800

    2024-02-27 04:07:39

--- doc.sh
+++ doc.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cd  /e/workspace/learn/docs
+git pull 
 git add . && git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
 git push origin HEAD:main
 
 cd ../
+git pull 
 git add . && git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
 git push origin
